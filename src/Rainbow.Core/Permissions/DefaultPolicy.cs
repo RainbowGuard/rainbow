f261a30453f29b9783629e2c48ed57533f74eb48
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-10-13 11:06:44 -0700

    Add basic permissions policies

---
+++ src/Rainbow.Core/Permissions/DefaultPolicy.cs
@@ -0,0 +1,10 @@
+﻿namespace Rainbow.Core.Permissions
+{
+    /// <summary>
+    /// Default permissions policy, unable to perform any privileged actions.
+    /// </summary>
+    public class DefaultPolicy : PermissionsPolicy
+    {
+        internal DefaultPolicy() { }
+    }
+}
---
+++ src/Rainbow.Core/Permissions/PermissionsPolicy.cs
@@ -0,0 +1,22 @@
+﻿namespace Rainbow.Core.Permissions
+{
+    public abstract class PermissionsPolicy
+    {
+        /// <summary>
+        /// Whether or not agents with this policy can flag users.
+        /// </summary>
+        public bool CanFlagUsers { get; protected init; }
+
+        /// <summary>
+        /// Whether or not agents with this policy can add users to the scam/bot account
+        /// list without flagging them.
+        /// </summary>
+        public bool CanListUsers { get; protected init; }
+
+        internal PermissionsPolicy() { }
+
+        public static readonly PermissionsPolicy Default = new DefaultPolicy();
+        public static readonly PermissionsPolicy Trusted = new TrustedPolicy();
+        public static readonly PermissionsPolicy Administrator = new AdministratorPolicy();
+    }
+}
