060ecf2362a0ec0853975bfa3c4e90e696ec0acc
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-10-13 20:32:33 -0700

    Expose each assembly to its test project

---
+++ src/Rainbow.Application/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Rainbow.Application.Tests")]
--- src/Rainbow.Discord/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Rainbow.Discord
-{
-    public class Class1
-    {
-    }
-}
