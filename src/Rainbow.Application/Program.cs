e2de67af6cf9c69b914fcc2d32512a14fdc9d741
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-10-15 22:22:13 -0700

    Sending the Junkyard to the Shadow Realm

--- src/Rainbow.Application/Program.cs
+++ src/Rainbow.Application/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Rainbow.Core;
 using Rainbow.Discord;
-using Rainbow.Junkyard;
 using System;
 using System.Threading.Tasks;
 
@@ -13,11 +12,7 @@ namespace Rainbow.Application
         {
             var services = new ServiceCollection();
 
-            var username = Environment.GetEnvironmentVariable("JUNKYARD_USERNAME");
-            var password = Environment.GetEnvironmentVariable("JUNKYARD_PASSWORD");
             var token = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN");
-
-            services.AddJunkyard("https://github.com/RainbowGuard/junkyard.git", username, password);
             services.AddDiscord(token);
 
             services.AddSingleton<CoreService>();
--- src/Rainbow.Core/Entities/FlaggedUser.cs
+++ src/Rainbow.Core/Entities/FlaggedUser.cs
@@ -1,9 +1,14 @@
-﻿namespace Rainbow.Core.Entities
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rainbow.Core.Entities
 {
     public class FlaggedUser : IEntity
     {
         public string Id { get; set; }
 
         public string FlagReason { get; set; }
+
+        private IList<Uri> Attachments { get; set; }
     }
 }
