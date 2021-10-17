a00f34a2b640c5cf12ec58e933fda14a75ea0b8d
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-10-17 09:44:39 -0700

    Set up config database, use host builder

---
+++ src/Rainbow.Application/Migrations/20211017164320_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rainbow.Application.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Guilds",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    SystemChannelId = table.Column<ulong>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guilds", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Guilds");
+        }
+    }
+}
---
+++ src/Rainbow.Core/Entities/GuildConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Rainbow.Core.Entities
+{
+    public class GuildConfiguration : IEntity
+    {
+        public string Id { get; set; }
+
+        public ulong SystemChannelId { get; set; }
+    }
+}
