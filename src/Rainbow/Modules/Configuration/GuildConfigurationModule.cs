a60b976b823756170e6818a7dd2e9db522ae07d0
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-12-19 23:37:51 -0800

    Add simple module for reference

---
+++ src/Rainbow/Modules/Configuration/GuildConfigurationModule.cs
@@ -0,0 +1,16 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Rainbow.Modules.Configuration;
+
+[RequireContext(ContextType.Guild)]
+public class GuildConfigurationModule : ModuleBase<SocketCommandContext>
+{
+    [Command("rbprefix")]
+    [RequireUserPermission(GuildPermission.Administrator)]
+    public async Task PrefixAsync(char prefix)
+    {
+        await ReplyAsync("Rainbow command prefix updated for this guild.");
+    }
+}
