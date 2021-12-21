85acdb990a4792dcf97387613000979e6358deda
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-12-20 21:44:09 -0800

    Migrate to MariaDB from SQLite Not being able to drop columns easily is a nonstarter.

--- src/Rainbow/Contexts/GuildConfigurationContext.cs
+++ src/Rainbow/Contexts/GuildConfigurationContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using Rainbow.Entities;
 
 namespace Rainbow.Contexts;
@@ -8,7 +9,7 @@ public class GuildConfigurationContext : DbContext
     public DbSet<GuildConfiguration> GuildConfigurations { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite("Data Source=config.db");
+        => options.UseMySql("Database=rainbow;Server=localhost;Port=3306;UID=rainbow;PWD=rainbow;", new MariaDbServerVersion(new Version(10, 5, 9)));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
